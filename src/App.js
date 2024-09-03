5ff48bd648014b3f564cc1ad31958fd2ea55ca96
hayetmnasri <hayetmnasri@hotmail.fr>
2024-09-03 09:58:52 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Slider from './components/Slider'
+import ProductList from './components/ProductList'
+import About from './components/About'
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div>
+    <div className='App'>
       <NavBar/>
-      <Slider/>
+      <Routes>
+        <Route path='/' element={<>
+                                  <Slider/> 
+                                   <ProductList/>
+                                   </>
+                                   }/>
+        <Route path='About' element={<About/>}/>
+      </Routes>
+      
+      
     </div>
   )
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,7 @@ function Product(props) {
   return (
     <div>
       <div className="card">
-       <img src={props.product.image} className="card-img-top" alt="..."/>
+       <img src={props.product.image} className="card-img-top" alt={props.product.title}/>
      <div className="card-body">
         <h5 className="card-title">{props.product.title}</h5>
        <p className="card-text">{props.product.description}</p>
